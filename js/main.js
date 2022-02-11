dd3ad0bc72204e67cae959b6003d00c9db143481
BlueMoon968 <lm.mastro96@gmail.com>
2022-02-11 16:04:09 +0100

    Added Navbar and Footer

--- js/main.js
+++ js/main.js
@@ -16,6 +16,11 @@
         })
     }
 
+    const SILVER = 0xcfc9d1
+    const MID_BLUE = 0x424572
+    const DARK_BLUE = 0x2f2d3b
+    const WHITE = 0xffffff
+
     //###############################################################################
     //
     // LOCALIZATION MANAGER
@@ -49,6 +54,15 @@
             this._events[e].push(f)
         }
 
+        static off(e, f) {
+            if(!!this._events[e]) {
+                if(this._events.indexOf(f) > -1) {
+                    this._events.splice(this._events.indexOf(f), 1)
+                }
+                
+            }
+        }
+
         static emit(e) {
             if(!!this._events[e]) {
                 for(const event of this._events[e]) {event()}
@@ -73,6 +87,124 @@
 
     //###############################################################################
     //
+    // TEXT
+    //
+    //###############################################################################
+
+    class Text extends PIXI.Text {
+
+        static DEFAULT_STYLE = {
+            fontFamily: "Oswald",
+            fontSize: 28,
+            fill: WHITE
+        }
+
+        constructor(key, style = "DEFAULT_STYLE") { 
+            super(Language.getString(key),Text[style])
+            this._key = key
+            Language.on(Language.LANGUAGE_CHANGE, this.updateLocalization.bind(this))
+        }
+
+        updateLocalization() {this.text = Language.getString(this._key)}
+
+        dispose() {
+            Language.off(Language.LANGUAGE_CHANGE, this.updateLocalization.bind(this))
+            this.parent.removeChild(this)
+        }
+
+        set fontSize(value) {this.style.fontSize = value}
+        get fontSize() {return this.style.fontSize}
+    }
+
+    //###############################################################################
+    //
+    // NAVBAR
+    //
+    //###############################################################################
+
+    class Navbar extends PIXI.Container {
+
+        constructor() {
+            super()
+            this._pen = new PIXI.Graphics()
+            this.addChild(this._pen)
+            this.drawBackground()
+            this.drawHeader()
+            this.addCommands()
+        }
+
+        drawBackground() {
+            const HEIGHT = Math.floor(App.height() / 15)
+            this._pen.beginFill(DARK_BLUE,0.5)
+            this._pen.drawRect(0,0,App.width(),HEIGHT)
+            this._pen.endFill()       
+            const THINLINE = 2 
+            this._pen.beginFill(MID_BLUE)
+            this._pen.drawRect(0,0,App.width(),THINLINE)
+            this._pen.drawRect(0,HEIGHT - THINLINE,App.width(),THINLINE)
+            this._pen.endFill()
+        }
+
+        drawHeader() {
+            let nebula = new Text("header")
+            const MARGIN = 24
+            nebula.fontSize = 36
+            nebula.anchor.set(0,0.5)
+            nebula.position.set(MARGIN*10, this.height / 2)
+            this.addChild(nebula)
+        }
+
+        addCommands() {
+            const COMMANDS = ["home", "projects", "about"]
+            for(let i = 0; i < COMMANDS.length; i++) {
+                const command = COMMANDS[i]
+                let text = new Text(command)
+                text.fontSize = 18
+                text.anchor.y = 0.5
+                text.buttonMode = true
+                text.interactive = true
+                let ww = i === 0 ? Math.floor(this.width / 4) : this.children[this.children.length - 1].x + this.children[this.children.length - 1].width + Math.floor(this.width / 24)
+                text.position.set(ww, this.height / 2)
+                this.addChild(text)
+            }
+        }
+    }
+
+    //###############################################################################
+    //
+    // FOOTER
+    //
+    //###############################################################################
+
+    class Footer extends PIXI.Container {
+
+        constructor() {
+            super()
+            this._pen = new PIXI.Graphics()
+            this.addChild(this._pen)
+            this.drawFooter()
+        }
+
+        drawFooter() {
+            const THINLINE = 3
+            this._pen.beginFill(MID_BLUE)
+            this._pen.drawRect(0,0,App.width(),THINLINE)
+            this._pen.endFill()
+            this._pen.beginFill(DARK_BLUE)
+            this._pen.drawRect(0,THINLINE,App.width(),Math.floor(App.height() / 10))
+            this._pen.endFill()
+
+            // Add texts
+            const disclaimer = new Text("disclaimer")
+            disclaimer.fontSize = 24
+            const MARGIN = 24
+            disclaimer.position.set(MARGIN*10, MARGIN)
+            this.addChild(disclaimer)
+        }
+    }
+
+    //###############################################################################
+    //
     // APP
     //
     //###############################################################################
@@ -91,15 +223,11 @@
         }
 
         static mainLoop() {
-            window.addEventListener('DOMContentLoaded', (event) => {
-                document.fonts.ready.then(() => {
-                    Language.init(() => {
-                        document.body.appendChild(this.application.view);
-                        this.initElements();
-                        this.ticker.add(this.update, this);
-                    })
-                })
-            });
+            Language.init(() => {
+                document.body.appendChild(this.application.view);
+                this.initElements();
+                this.ticker.add(this.update, this);
+            })
         }
 
         static defaultStyle(size = 28, fill = "white") {
@@ -122,7 +250,7 @@
             italian.alpha = Language.isLocale("it") ? 1 : 0.7;
             italian.position.set(MARGIN, MARGIN);
             italian.buttonMode = true
-            this.add(italian)
+            
             const english = new PIXI.Text("ENGLISH", this.defaultStyle(18));
             english.interactive = true;
             english.on("pointerdown", () => {
@@ -131,10 +259,11 @@
             english.alpha = Language.isLocale("en") ? 1 : 0.7;
             english.position.set(italian.x + english.width + 5,italian.y);
             english.buttonMode = true
-            this.add(english)
+            
 
             // TEMPORARY
-            const wip = new PIXI.Text(Language.getString("wip"), this.defaultStyle(48));
+            const wip = new Text("wip")
+            wip.fontSize = 48
             this.add(wip);
             wip.anchor.set(0.5)
             wip.position.set(this.width()/2,this.height()/2)
@@ -142,8 +271,17 @@
             Language.on(Language.LANGUAGE_CHANGE, () => {
                 italian.alpha = Language.isLocale("it") ? 1 : 0.7;
                 english.alpha = Language.isLocale("en") ? 1 : 0.7;
-                wip.text = Language.getString("wip")
             })
+
+            // FOOTER
+            const footer = new Footer()
+            footer.position.set(0, this.height() - footer.height)
+            this.add(footer)
+
+            const navbar = new Navbar()
+            this.add(navbar)
+            this.add(italian)
+            this.add(english)
             
         }
 
